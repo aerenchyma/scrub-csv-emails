3ed708d5e3090ddf81f8e15a036dd51e0fb4fda7
Brian Dant <brian.r.dant@gmail.com>
2012-10-05 14:21:49 -0400

    Debugging version

--- scrub_csv_emails/scrub_csv_emails.py
+++ scrub_csv_emails/scrub_csv_emails.py
@@ -15,191 +15,142 @@ import sys
 # This will also determine the order of function calls.
 
 
+class CleanCsv(object):
+    # change this to just take and unpack args
+    def __init__(self, args):
+        self.in_file = args[1]  # Storing original (necc?)
+        self.clean_out_file = args[2]
+        self.dirty_out_file = args[3]
+        self.flags = [x for x in args[4:] if sys.argv != ""]
+        # Bug?  sys.argv will never be blank         ^^^
+        self.manual_repair = []
+        self.rows = []
+        self.functions = [
+            'strip_blank_fields',
+            'strip_whitespace',
+            'capitalize',
+            'strip_blank_lists',
+            'split_on_blanks',
+            'remove_duplicate_names',
+            'remove_bad_rows',
+            'columnize',
+            ]
 
-def grab_file_data(in_file, out_good_file, out_bad_file):
-    print "---------"
-    print "running grab_file_data()"
-    raw_input()
-    with open(in_file, 'rt') as opened_file:
-        read_file = csv.reader(opened_file)
-        rows = []
-        for row in read_file:
-            rows.append(row)
-        return rows, out_good_file, out_bad_file
-        opened_file.close
+    def do_scrub(self):
+            self.fxns = [x for x in self.functions if x not in self.flags]
+            self.rows = self.grab_file_data(self.in_file)
+            for fx in self.fxns:
+                print "------"
+                print "in the for fx in self.fxns"
+                print "self.fxns == %r" % self.fxns
+                print "fx == %r" % fx
+                raw_input()
+                next_operation = getattr(self, fx)
+                print "--------"
+                print "got the attr: next_operation == %r" % next_operation
+                print "self.rows? %r" % self.rows
+                raw_input()
+                next_operation()
+            if self.manual_repair:
+                self.dirty_out_file_complete = self.write_csv(self.manual_repair, self.dirty_out_file)
+            self.clean_out_file_complete = self.write_csv(self.rows, self.in_file)
 
+    def grab_file_data(self, filename):
+        with open(filename, 'rt') as opened_file:
+            read_file = csv.reader(opened_file)
+            for row in read_file:
+                self.rows.append(row)
+            opened_file.close
+            print "----------"
+            print "closed the file"
+            print "self.rows? %r" % bool(self.rows)
+            print "self.rows[0]: %r" % self.rows[0]
+            raw_input()
 
-def strip_blank_fields(rows):
-    print "---------"
-    print "running strip_blank_fields()"
-    print "rows? %r" % bool(rows)
-    raw_input()
-
-    for row in rows:
-        while "" in row:
-            row.remove("")
-    return rows
-
-
-def strip_whitespace(rows):
-    print "---------"
-    print "running strip_whitespace()"
-    print "rows? %r" % bool(rows)
-    raw_input()
-    for row in rows:
-        for num, field in enumerate(row):
-            row[num] = field.strip()
-    return rows
-
-
-def capitalize(rows):
-    print "---------"
-    print "running capitalize()"
-    print "rows? %r" % bool(rows)
-    raw_input()
-    for row in rows:
-        for num, field in enumerate(row):
-            if "@" not in field and not field.istitle():
-                row[num] = field.title()
-    capitalized_list = rows
-    return capitalized_list
-
-
-def strip_blank_lists(rows):
-    print "---------"
-    print "running strip_blank_lists()"
-    print "rows? %r" % bool(rows)
-    raw_input()
-    for row in rows:
-        if row == []:
-            rows.remove(row)
-    return rows
-    # why not this:
-    # while [] in rows:
-    #    rows.remove([])
-
-
-def split_on_blanks(rows):
-    print "---------"
-    print "running split_on_blanks()"
-    print "rows? %r" % bool(rows)
-    raw_input()
-    for row in rows:
-        for num, field in enumerate(row):
-            if ' ' in field:
-                x = field.split(' ')
-                row.pop(num)
-                for i in x:
-                    row.append(i)
-
-    for row in rows:
-        for num, field in enumerate(row):
-            if "@" in field:
-                x = row.pop(num)
-                row.append(x)
-
-    return rows
-
-
-def remove_duplicate_names(rows):
-    print "---------"
-    print "running remove_duplicate_names()"
-    print "rows? %r" % bool(rows)
-    raw_input()
-    for num, row in (enumerate(rows)):
-        for x in xrange(len(row)):
-            if row.count(row[x]) > 1:
-                row.pop(x)
-                break
-    return rows
-
-
-def remove_bad_rows(rows):
-    print "---------"
-    print "running remove_bad_rows()"
-    print "rows? %r" % bool(rows)
-    raw_input()
-    global manual_repair
-
-  # Remove all rows that don't have at least three slots filled
-    for num, row in enumerate(rows):
-        if len(row) < 3:
-            x = rows.pop(num)
-            manual_repair.append(x)
-
-    # Remove all rows that don't have an email address.
-    for num, row in enumerate(rows):
-        bad = True
-        for field in row:
-            if '@' in field:
-                bad = False
-                break
-        if bad == True:
-            x = rows.pop(num)
-            manual_repair.append(x)
-
-    return rows
-
-def columnize(rows):
-    print "---------"
-    print "running columize()"
-    print "rows? %r" % bool(rows)
-    raw_input()
-    titles = [
-    'Mr', 'Mrs', 'Mr.', 'Mrs.', 'mr',
-    'mrs', 'mr.', 'mrs.', 'Miss', 'miss'
-    ]
-
-    for row in rows:
-        print "-----------"
-        print "top of for loop"
-        print "row = %r " % row
+    def strip_blank_fields(self):
+        print "--------"
+        print "in the top of the strip_blank_fields()"
+        print "self.rows? %r" % bool(self.rows)
         raw_input()
-        if set(row).isdisjoint(set(titles)):
-            row.insert(0, '')
+        for row in self.rows:
+            while "" in row:
+                row.remove("")
 
-    return rows
+    def strip_whitespace(self):
+        for row in self.rows:
+            for num, field in enumerate(row):
+                row[num] = field.strip()
 
+    def capitalize(self):
+        for row in self.rows:
+            for num, field in enumerate(row):
+                if "@" not in field and not field.istitle():
+                    row[num] = field.title()
 
-def write_csv(rows, filename):
-    print "---------"
-    print "running write_csv()"
-    print "rows? %r" % bool(rows)
-    raw_input()
-    f = open(filename, 'wt')
-    try:
-        writer = csv.writer(f)
-        writer.writerow(('Title', 'First', 'Middle', 'Last', 'Email'))
-        for row in rows:
-            writer.writerow(row)
-    finally:
-        f.close()
+    def strip_blank_lists(self):
+        while [] in self.rows:
+            self.rows.remove([])
 
+    def split_on_blanks(self):
+        for row in self.rows:
+            for num, field in enumerate(row):
+                if ' ' in field:
+                    x = field.split(' ')
+                    row.pop(num)
+                    for i in x:
+                        row.append(i)
 
-# Iterate through the list of functions and execute them in order of the list.
-if __name__ == "__main__":
-    global functions
-    global manual_repair
-    functions = [
-        'strip_blank_fields',
-        'strip_whitespace',
-        'capitalize',
-        'strip_blank_lists',
-        'split_on_blanks',
-        'remove_duplicate_names',
-        'remove_bad_rows',
-        'columnize',
+        for row in self.rows:
+            for num, field in enumerate(row):
+                if "@" in field:
+                    x = row.pop(num)
+                    row.append(x)
+
+    def remove_duplicate_names(self):
+        for num, row in (enumerate(self.rows)):
+            for x in xrange(len(row)):
+                if row.count(row[x]) > 1:
+                    row.pop(x)
+                    break
+
+    def remove_bad_rows(self):
+      # Remove all rows that don't have at least three slots filled
+        for num, row in enumerate(self.rows):
+            if len(row) < 3:
+                x = self.rows.pop(num)
+                self.manual_repair.append(x)
+
+        # Remove all self.rows that don't have an email address.
+        for num, row in enumerate(self.rows):
+            bad = True
+            for field in row:
+                if '@' in field:
+                    bad = False
+                    break
+            if bad == True:
+                x = self.rows.pop(num)
+                self.manual_repair.append(x)
+
+    def columnize(self):
+        titles = [
+        'Mr', 'Mrs', 'Mr.', 'Mrs.', 'mr',
+        'mrs', 'mr.', 'mrs.', 'Miss', 'miss'
         ]
 
-# Strip out all functions that were passed in as argunments (from command line)
-# as false.
+        for row in self.rows:
+            if set(row).isdisjoint(set(titles)):
+                row.insert(0, '')
 
-    manual_repair = []
-    rows, good_file, file_to_repair = grab_file_data(sys.argv[1], sys.argv[2], sys.argv[3])
-    for function in functions:
-        next_operation = globals()[function]
-        rows = next_operation(rows)
-    write_csv(rows, good_file)
-    if manual_repair:
-        write_csv(manual_repair, file_to_repair)
+    def write_csv(self, rows, name_to_write):
+        f = open(name_to_write, 'wt')
+        try:
+            writer = csv.writer(f)
+            writer.writerow(('Title', 'First', 'Middle', 'Last', 'Email'))
+            for row in self.rows:
+                writer.writerow(row)
+        finally:
+            f.close()
 
+if __name__ == "__main__":
+    x = CleanCsv(sys.argv)
+    x.do_scrub()
